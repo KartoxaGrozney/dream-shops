3e2b8a94b36c1c6322b33ee5e664a2bd00916616
Kartoxa <kartoxa.sosynok@gmail.com>
2025-11-13 19:55:49 +0300

    add cart, cartItem, user entities

---
+++ src/main/java/com/kartoxa/dreamshops/model/Cart.java
@@ -0,0 +1,18 @@
+package com.kartoxa.dreamshops.model;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> items;
+}
---
+++ src/main/java/com/kartoxa/dreamshops/model/CartItem.java
@@ -0,0 +1,20 @@
+package com.kartoxa.dreamshops.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private int quantity;
+}
