81cdc182e8fd28b3def0c548d48392f18c2a345f
Kartoxa <kartoxa.sosynok@gmail.com>
2025-11-18 12:56:18 +0300

    add user, order, orderItem entities

---
+++ src/main/java/com/kartoxa/dreamshops/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.kartoxa.dreamshops.enums;
+
+public enum OrderStatus {
+    PENDING,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}
--- src/main/java/com/kartoxa/dreamshops/model/User.java
+++ src/main/java/com/kartoxa/dreamshops/model/User.java
@@ -1,24 +1,34 @@
 package com.kartoxa.dreamshops.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name="users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Order> orders;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Cart cart;
 
     private String userName;
 }
